e01ac1afdc2d41c9306e0c15b2d88871a5d261cd
Beatriz Abreu <beahabreu@gmail.com>
2026-03-17 11:45:00 -0300

    Feat: Criação das rotas PUT e DELETE

--- backend/handlers/symbol_handlers.go
+++ backend/handlers/symbol_handlers.go
@@ -7,16 +7,14 @@ import (
   "loquor-sign/models"
 )
 
-func GetSymbols (c echo.Context) error {
-	
-	symbols := []models.Symbol{
+var symbols = []models.Symbol{
 		{ID:"1", Name:"Água", Image:"agua.png", Category_ID:"1"},
 		{ID:"2", Name:"Comer", Image:"comer.png", Category_ID:"2"},
 		{ID:"3", Name:"Dormir", Image:"dormir.png", Category_ID:"3"},
 	}
 
+func GetSymbols (c echo.Context) error {
 	return c.JSON(http.StatusOK, symbols)
-
 }
 
 func CreateSymbol (c echo.Context) error{
@@ -28,5 +26,47 @@ func CreateSymbol (c echo.Context) error{
 		})
 	}
 
+	symbols = append(symbols, symbol)
+
 	return c.JSON(http.StatusCreated, symbol)
+}
+
+func UpdateSymbol (c echo.Context) error{
+	id := c.Param("id")
+
+	var updateSymbol models.Symbol
+
+	if err := c.Bind(&updateSymbol); err != nil{
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "dados inválidos",
+		})
+	}
+
+	for i, symbol := range symbols{
+		if symbol.ID == id {
+			symbols[i] = updateSymbol
+			return c.JSON(http.StatusOK, updateSymbol)
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{
+		"error": "Símbolo não encontrado",
+	})
+}
+
+func DeleteSymbol (c echo.Context) error{
+	id := c.Param("id")
+
+	for i, symbol := range symbols{
+		if symbol.ID == id {
+			symbols = append(symbols[:i], symbols[i+1:]...)
+			return c.JSON(http.StatusOK, map[string]string{
+				"message": "símbolo deletado com sucesso",
+			})
+		}
+	}
+
+	return c.JSON(http.StatusNotFound, map[string]string{
+		"error": "símbolo não encontrado",
+	})
 }
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -8,4 +8,6 @@ import (
 func InitRoutes(e *echo.Echo){
 	e.GET("/symbols", handlers.GetSymbols)
 	e.POST("/symbols", handlers.CreateSymbol)
+	e.PUT("/symbols/:id", handlers.UpdateSymbol)
+	e.DELETE("symbols/:id", handlers.DeleteSymbol)
 }
